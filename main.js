0d82eef03928c4143065b6fc338d2b00d639e652
Christian <christianmatamoros98@gmail.com>
2020-11-23 04:47:58 -0500

    commit

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-const grid = new Muuri('.grid',{
-    layout: { 
+const grid = new Muuri('.grid', {
+    layout: {
         rounding: false
     }
 });
-window.addEventListener('load',() => {
+window.addEventListener('load', () => {
     grid.refreshItems().layout();
     document.getElementById('grid').classList.add('img-load');
+
+    const enlaces = document.querySelectorAll('.categorias a');
+    enlaces.forEach((enlace) => {
+        enlace.addEventListener('click', (ev) => {
+            ev.preventDefault();
+            enlaces.forEach((enl) => enl.classList.remove.add('activo'));
+            ev.target.classList.add('activo');
+        });
+    });
 });
